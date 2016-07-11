1493324466ba1b6c6c55d2392a59d2e3396734c7
fis-trongdd2 <trongdd95@gmail.com>
2016-07-11 13:37:24 +0700

    opti

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,9 @@
 package main;
 
 import data.ARFF;
-import data.GhiToHop;
-import data.ToHopNhom;
 import data.XML;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by trong_000 on 6/29/2016.
--- src/main/TheHien.java
+++ src/main/TheHien.java
@@ -1,6 +1,5 @@
 package main;
 
-import data.ToHopNhom;
 import data.XML;
 
 import java.util.ArrayList;
